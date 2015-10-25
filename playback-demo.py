a58a9d1c2d013637575d160324b7dc885749e275
Fabian Chan <fabeschan@gmail.com>
2015-10-24 22:56:07 -0700

    First version of playback-demo.py

--- playback-demo.py
+++ playback-demo.py
@@ -1,17 +1,22 @@
 import data
 import timeit, time
 import mido
+from pprint import pprint
 from simplecoremidi import send_midi
+import random
 
-musicpiece = data.piece('output.mid')
-notes = musicpiece.unified_track.notes
+musicpieces = [data.piece('output.mid'), data.piece('mid/froglegs.mid')]
+notes = [mp.unified_track.notes for mp in musicpieces]
 
 class Event(object):
     def __init__(self, msg, pos):
         self.msg = msg
         self.pos = pos
 
-def convert_to_events(notes):
+    def __repr__(self):
+        return str((self.pos, self.msg))
+
+def convert_to_events(notes, d):
     events = []
     for n in notes:
         msg = mido.Message('note_on', note=n.pitch, channel=n.chn)
@@ -20,12 +25,31 @@ def convert_to_events(notes):
 
         msg = mido.Message('note_off', note=n.pitch, channel=n.chn)
         e1 = Event(msg, n.pos + n.dur)
-        events.append(e1)
+        #events.append(e1)
+
+        d[e0] = e1
     return events
 
+def read_trigger_file():
+    filename = 'trigger_file'
+    text = ''
+    try:
+        with open(filename, 'r+') as f:
+            text = f.read().strip()
+            f.seek(0)
+            f.truncate()
+    except:
+        print 'warn: cannot open trigger_file'
+    return text
+
 def main():
-    events = convert_to_events(notes)
-    events.sort(key=lambda x: x.pos)
+    d = {}
+    unended = {}
+    events_mp = [ convert_to_events(n, d) for n in notes ]
+    for e in events_mp:
+        e.sort(key=lambda x: x.pos)
+
+    events = random.choice(events_mp)
     start_time = time.clock()
 
     # loop
@@ -33,15 +57,29 @@ def main():
     i = 0
     count = 0
     while loop:
+        text = read_trigger_file()
+        if text:
+            print 'read triggerfile:', text
+            events = random.choice(events_mp)
+            start_time = time.clock()
+            i = 0
         cur_time = time.clock()
         pos = int((cur_time - start_time) * 1000000) / 3000
         if i < len(events):
             e = events[i]
+            things_to_delete = []
+            for k, v in unended.iteritems():
+                if k.pos < pos:
+                    send_midi(k.msg.bytes())
+                    things_to_delete.append(k)
+            for k in things_to_delete:
+                del unended[k]
+            print len(unended)
+
             if e.pos < pos:
                 send_midi(e.msg.bytes())
-                #send_midi((0x90, 0x3c, 0x40))
+                unended[d[e]] = e
                 i += 1
-                #print '={}'.format(i)
         else:
             loop = False
             print '{}/{}'.format(i, count)
