a98d8e53f944073c29f4137260140d989350c747
Fabian Chan <fabeschan@gmail.com>
2015-10-25 11:43:33 -0700

    Toggle between sources
    (still has some bugs)

--- playback-demo.py
+++ playback-demo.py
@@ -5,7 +5,7 @@ from pprint import pprint
 from simplecoremidi import send_midi
 import random
 
-musicpieces = [data.piece('output.mid'), data.piece('mid/froglegs.mid')]
+musicpieces = [data.piece('mid/owl.mid'), data.piece('mid/lost.mid')]
 notes = [mp.unified_track.notes for mp in musicpieces]
 
 class Event(object):
@@ -16,7 +16,7 @@ class Event(object):
     def __repr__(self):
         return str((self.pos, self.msg))
 
-def convert_to_events(notes, d):
+def convert_to_events(notes, note_offs):
     events = []
     for n in notes:
         msg = mido.Message('note_on', note=n.pitch, channel=n.chn)
@@ -25,9 +25,8 @@ def convert_to_events(notes, d):
 
         msg = mido.Message('note_off', note=n.pitch, channel=n.chn)
         e1 = Event(msg, n.pos + n.dur)
-        #events.append(e1)
 
-        d[e0] = e1
+        note_offs[e0] = e1
     return events
 
 def read_trigger_file():
@@ -39,51 +38,57 @@ def read_trigger_file():
             f.seek(0)
             f.truncate()
     except:
-        print 'warn: cannot open trigger_file'
+        raise # replace with pass if this is causing you problems
     return text
 
-def main():
-    d = {}
-    unended = {}
-    events_mp = [ convert_to_events(n, d) for n in notes ]
-    for e in events_mp:
-        e.sort(key=lambda x: x.pos)
+def apply_unended(unended, pos, now=False):
+    things_to_delete = []
+    for k in unended:
+        if now or k.pos < pos:
+            send_midi(k.msg.bytes())
+            things_to_delete.append(k)
+    for k in things_to_delete:
+        unended.remove(k)
 
-    events = random.choice(events_mp)
+def main():
+    # init
+    tempo_reciprocal = 3000
+    msg = mido.Message('note_on', note=60, channel=0)
+    send_midi(msg.bytes())
+    raw_input('Setting up channel. Please reset MIDI devices before continuing. Press [Enter]')
+    msg = mido.Message('note_off', note=60, channel=0)
+    send_midi(msg.bytes())
+
+    note_offs = {}
+    unended = set()
+    events_mp = [ convert_to_events(n, note_offs) for n in notes ]
+
+    event_idx = 0
+    events = events_mp[event_idx]
     start_time = time.clock()
 
     # loop
     loop = True
-    i = 0
-    count = 0
+    i = [0] * len(events)
     while loop:
         text = read_trigger_file()
         if text:
             print 'read triggerfile:', text
-            events = random.choice(events_mp)
-            start_time = time.clock()
-            i = 0
-        cur_time = time.clock()
-        pos = int((cur_time - start_time) * 1000000) / 3000
-        if i < len(events):
-            e = events[i]
-            things_to_delete = []
-            for k, v in unended.iteritems():
-                if k.pos < pos:
-                    send_midi(k.msg.bytes())
-                    things_to_delete.append(k)
-            for k in things_to_delete:
-                del unended[k]
-            print len(unended)
+            event_idx = (event_idx + 1) % 2
+            events = events_mp[event_idx]
+            apply_unended(unended, pos, now=True)
 
+        cur_time = time.clock()
+        pos = int((cur_time - start_time) * 1000000) / tempo_reciprocal
+        if i[event_idx] < len(events):
+            e = events[i[event_idx]]
             if e.pos < pos:
                 send_midi(e.msg.bytes())
-                unended[d[e]] = e
-                i += 1
+                unended.add(note_offs[e])
+                i[event_idx] += 1
+            apply_unended(unended, pos)
         else:
             loop = False
-            print '{}/{}'.format(i, count)
-        count += 1
 
 if __name__ == '__main__':
     main()
