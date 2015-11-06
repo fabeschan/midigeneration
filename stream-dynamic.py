b77fb0547b02b40b392e2fa7882c5697aebe5923
Fabian Chan <fabeschan@gmail.com>
2015-11-05 19:36:37 -0800

    stream-dynamic.py now receives mid filename as signal and schedules next cmm

--- stream-dynamic.py
+++ stream-dynamic.py
@@ -32,7 +32,7 @@ class DynamicMarkovNoteStateGenerator(object):
             print "End!"
     '''
 
-    def __init__(self, mm=None, seed=[]):
+    def __init__(self, mm=None, markov_models={}, seed=[]):
         '''
         Initialize the DynamicMarkovNoteStateGenerator with an empty or already existing and trained Markov model
 
@@ -48,6 +48,14 @@ class DynamicMarkovNoteStateGenerator(object):
             self.mm = mm
 
         self.buf = self.mm.get_start_buffer(seed)
+        self.markov_models = markov_models
+        self.next_mm = None
+
+    def get_model_name(self):
+        for k, v in markov_models.iteritems():
+            if self.mm == v:
+                return k
+        return None
 
     def next_transition_exists(self, mm=None):
         if mm is None:
@@ -58,30 +66,28 @@ class DynamicMarkovNoteStateGenerator(object):
             return False
 
     def replace_markov_model(self, mm):
-        if self.nextTransitionExists(mm):
-            self.mm = mm
-            return True
-        else:
-            return False
+        self.mm = mm
+
+    def set_next_markov_model(self, mm):
+        self.next_mm = mm
 
     def next_state_generator(self):
-        elem = self.mm.generate_next_state(self.buf)
-        yield elem
-
+        elem = None
         while elem != cmm.Markov.STOP_TOKEN:
-            self.buf = self.mm.shift_buffer(self.buf, elem)
+            if self.next_mm and self.next_transition_exists(self.next_mm):
+                self.replace_markov_model(self.next_mm)
+                self.next_mm = None
+                print 'replace model with [{}]'.format(self.get_model_name())
             elem = self.mm.generate_next_state(self.buf) # generate another
             if elem != cmm.Markov.STOP_TOKEN:
+                self.buf = self.mm.shift_buffer(self.buf, elem)
                 yield elem
-
-def handle_trigger(trigger_text, nsgen):
-    pass
 
 if __name__ == "__main__":
     # load MIDI files (from program arguments if provided)
     filenames = sys.argv[1:]
     if not filenames:
-        filenames = ["mid/easywinners.mid", "mid/froglegs.mid", "mid/hilarity.mid", "mid/lost.mid", "mid/owl.mid", "mid/sjeugen.mid"]
+        filenames = ["mid/easywinners.mid", "mid/froglegs.mid", "mid/hilarity.mid", "mid/sjeugen.mid"]
     musicpieces = {f: data.piece(f) for f in filenames}
 
     # train a markov model on each piece
@@ -89,8 +95,9 @@ if __name__ == "__main__":
     markov_models = {f: cmm.piece_to_markov_model(piece, segmentation=False, all_keys=False) for f, piece in musicpieces.iteritems()}
 
     # initialize the note state generator with a random initial markov model
-    initial_markov = random.choice(markov_models.values())
-    nsgen = DynamicMarkovNoteStateGenerator(initial_markov).next_state_generator()
+    initial_markov = random.choice(markov_models.keys())
+    dmnsg = DynamicMarkovNoteStateGenerator(markov_models[initial_markov], markov_models)
+    nsgen = dmnsg.next_state_generator()
 
     # init some parameters
     tempo_reciprocal = 1500 # 'speed' of playback. need to adjust this carefully, and by trial and error
@@ -109,11 +116,21 @@ if __name__ == "__main__":
         trigger_text = playback.read_trigger_file('trigger_file')
         if trigger_text:
             print 'received trigger text: [{}]'.format(trigger_text)
-            handle_trigger(trigger_text, nsgen)
+            if trigger_text in markov_models.keys():
+                dmnsg.set_next_markov_model(markov_models[trigger_text])
+                print 'set next model: [{}]'.format(trigger_text)
+            elif trigger_text == 'random':
+                model_names = markov_models.keys()[:]
+                model_names.remove(dmnsg.get_model_name())
+                next_model = random.choice(model_names)
+                dmnsg.set_next_markov_model(markov_models[next_model])
+                print 'set next model: [{}]'.format(next_model)
+
 
         playback_pos = int((cur_time - start_time) * 1000000) / tempo_reciprocal
 
         if playback_pos >= next_pos:
+            print 'current model name: [{}]'.format(dmnsg.get_model_name())
             note_state = next(nsgen, None) # generate the next note_state
             if note_state:
                 notes, next_pos = note_state.to_notes(bar, next_pos)
